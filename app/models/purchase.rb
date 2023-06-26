43081d10408796bf2bbf05c5650ddacbf667d0ea
Emerson Filho <emersonfilho3@gmail.com>
2023-06-26 08:57:07 -0300

    Update purchase.rb
    Organizando lógica para model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,4 +4,20 @@ class Purchase < ApplicationRecord
     has_many :products, through: :purchase_items
   
     accepts_nested_attributes_for :purchase_items
+
+    def self.montly_buyer_listing
+        query = <<-SQL
+        SELECT strftime('%Y-%m', purchases.created_at) AS month,
+                clients.name AS user_name,
+                MAX(purchases.total_value) AS total_spent
+        FROM purchases
+        JOIN clients ON clients.id = purchases.client_id
+        GROUP BY month
+        SQL
+    
+        results = ActiveRecord::Base.connection.execute(query)
+        resultFilter = results.map { |row| { month: row['month'], user_name: row['user_name'], total_spent: row['total_spent'] } }
+        resultFilter
+    end
+
 end
